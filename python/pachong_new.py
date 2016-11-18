2aea95cb83e5ff6e89782876c2e5b8cce9c07c3c
unknown <you@example.com>
2016-11-18 20:05:14 +0800

    python

---
+++ python/pachong_new.py
@@ -0,0 +1,57 @@
+#!E:\python\python.exe
+# -*- coding: UTF-8 -*-
+print "Content-type:text/html"
+print
+import urllib
+import urllib2
+import re
+import MySQLdb
+
+class News:
+
+    #init
+    def __init__(self):
+        self.url = "http://news.baidu.com/"
+
+    #convert div to ''
+    def tranTags(self, x):
+        pattern = re.compile('<div.*?</div>')
+        res = re.sub(pattern, '', x)
+        return res
+
+    #getPage
+    def getPage(self):
+        url = self.url
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+        return response.read()
+
+    #get navCode
+    def getNewCode(self):
+        page = self.getPage()
+        pattern = re.compile('(<div id="pane-news".*?)<ul id="goTop" class="mod-sidebar">', re.S)
+        newCode = re.search(pattern, page)
+        return newCode.group(1)
+    #get title
+    def getTitle(self):
+        title = self.getNewCode()
+        pattern = re.compile('<a href="(http://.*?/).*?>(.*?)</a>', re.S)
+        itmes = re.findall(pattern, title)
+        for item in itmes:
+             return itmes
+# 打开数据库连接
+db = MySQLdb.connect("localhost","root","root","test",charset="GBK")
+cursor = db.cursor()
+newi = News()
+new = newi.getTitle()
+
+for i in new:
+    print i[0],newi.tranTags(i[1])
+    n_name = newi.tranTags(i[1])
+    sql = """INSERT INTO news_nav(n_name,n_url)VALUES(%s, %s )""" % ("'"+n_name+"'","'"+i[0]+"'")
+    #vala = (vall,val[0])
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
